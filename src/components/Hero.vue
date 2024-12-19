e2ec19d504f87b45d4f8594057be47f2518b025c
Salawudeen Abdultawab <abdultawabsalawudeen@gmail.com>
2024-12-19 17:09:07 +0100

    nav fix

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -4,14 +4,14 @@
 <template>
   <div class="hero-image">
     <div class="hero-text h-full flex flex-col w-full justify-center items-center ">
-            <h1 class="text-[2.2rem] md:text-[2.45rem] text-center cabin  lg:text-[3.831rem]  font-semibold animate__animated animate__fadeIn animate__delay-1s">GeoHazards; Predict, Prepare, and Protect.</h1>
+            <h1 class="text-[2.2rem] md:text-[2.45rem] text-center cabin  lg:text-[3.831rem]  font-semibold">GeoHazards; Predict, Prepare, and Protect.</h1>
 
-            <p class="M20 white text-[0.8rem] md:text-[1.4rem] merri leading-[2rem] text-center animate__animated animate__fadeIn animate__delay-1s" style="margin-bottom: 16px;"
+            <p class="M20 white text-[0.8rem] md:text-[1.4rem] merri leading-[2rem] text-center" style="margin-bottom: 16px;"
             
             >Promoting a sustainable environment through the           development, generating applications, and <br/>
             dissemination of Geohazard maps using advanced Geospatial Technology and Artificial Intelligence
             </p>
-            <div class="flex gap-4 flex-col md:flex-row animate__animated animate__fadeIn animate__delay-1s">
+            <div class="flex gap-4 flex-col md:flex-row">
             <router-link class="" :to="{ name: 'floodEvent'}">
               <button class=" text-lg bg-brandgreen transitionAll rounded-[0.5rem] px-10 py-4 w-full">
                 View Flood Maps
