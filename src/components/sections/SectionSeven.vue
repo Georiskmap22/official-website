032d2228b47b64ec0f5e0b8e3a4bb03642c3e7bc
Salawudeen Abdultawab <abdultawabsalawudeen@gmail.com>
2025-01-02 23:27:37 +0100

    UNDRR url update

--- src/components/sections/SectionSeven.vue
+++ src/components/sections/SectionSeven.vue
@@ -2,8 +2,11 @@
     <section class="w-full bg-[#FFFFFF] py-[3.75rem]">
         <div class="w-[83.47%] mx-auto">
             <h3 class="text-[#0E1C16] font-merri font-[700] text-[1.94rem] leading-[2.3rem] mb-[2rem] midDesk:text-[1.5rem] text-center">
-                To support GRMI's efforts in empowering children and communities to build resilience against flooding, we encourage you to donate to our organization. Your contribution will help fund our educational programs, community mapping initiatives, and disaster preparedness efforts across flood-prone regions of Nigeria.
+                GRMI's work reflects 
+                <a href="https://www.undrr.org" target="_blank" 
+                class="rotate-link bg-[#4E695D] rounded-[100px] border border-[#0E1C16] py-[0.63rem] px-[1.25rem] text-[#FFFFFF] inline-block transitionAll hover:rotate-[-5deg]">UNDRR's</a> global priorities in child-centered disaster risk reduction. Our approach aligns with UNDRR's guideline for engaging children and youth in disaster risk reduction and resilience building. To support GRMI's effort in empowering children and communities to build resilience against flooding, we encourage you to donate to our organization. Your contribution will help fund our educational programs in community regions of Nigeria.
             </h3>
+
             <div class="w-full grid place-items-center">
                 <button class="bg-[#0E1C16] rounded-[100px] py-[1rem] px-[2.5rem] midDesk:px-[3rem]">
                     <h4 class="text-[#FFFFFF] font-merri text-[1.25rem] leading-[1.5rem] ">Donate</h4>
@@ -17,6 +20,7 @@
 
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
+  
 
 </style>
